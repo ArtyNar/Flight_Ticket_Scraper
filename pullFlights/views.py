921243645e07a0720dcaa4998abd86b00506dc23
Artem <arty.nar00@gmail.com>
2022-12-13 15:03:43 -0700

    Core is workinggit add .git add .

--- pullFlights/views.py
+++ pullFlights/views.py
@@ -3,30 +3,75 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 from django.http import HttpResponseRedirect
+
+# Selenium imports
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from time import sleep
 
+# Our scraper!
 def scrape(request):
-    site = 'https://www.kayak.com/flights/SLC-LAX/2023-01-12/2023-01-19s'
+    # User input about a desired flight
+    origin = "SLC"
+    destination = "LAX"
+    startdate = "2023-01-06"
+    #enddate = "2023-01-09"
 
+    # Assembling the url
+    url = "https://www.kayak.com/flights/" + origin + "-" + destination + "/" + startdate # + "/" + enddate
+    
+    # Loading the selenium driver.
+    # It will open a chorme page, that will accomplish the scraping
     chrome_options = webdriver.ChromeOptions()
     driver = webdriver.Chrome("chromedriver.exe")
     driver.implicitly_wait(20)
-    driver.get(site)
-
-    captcha = driver.find_element(By.XPATH, "//iframe[@role='presentation']")
-    captcha.click()
-
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    data = soup.title
-
-    # # Creates a session
-    # session = requests.Session()
-    # session.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    driver.get(url)
     
-    # page = session.get(site)
-    # soup = BeautifulSoup(page.text, 'html.parser')
+    # From what I udnerstand, it allows the page to load before we proceed
+    # I might be wrong though
+    sleep(2)
+
+
+    # Here, we just load more and more results on a page.
+    # Technically, we can do it any number of times
+    for i in range(5):
+        try:
+            more_btn = driver.find_element(By.XPATH, "//a[contains(text(), 'Show more results')]")
+            more_btn.click()
+            sleep(2)
+        except:
+            continue
+
+    # Now, "souping" the data
+    soup=BeautifulSoup(driver.page_source, 'lxml')
+
+    deptimes = soup.find_all('span', attrs={'class': 'depart-time base-time'})
+
+    # These are ALL of the meridiems. Have to split them for
+    # the ones corresponding to departures and ones to arrivals
+    meridiem = soup.find_all('span', attrs={'class': 'time-meridiem meridiem'})
+    mer_dept = meridiem[::2]
+    mer_arr = meridiem[1::2]
+
+
+
+    arrtimes = soup.find_all('span', attrs={'class': 'arrival-time base-time'})
+    stops = soup.find_all('span', attrs={'class': 'stops-text'})
+    prices = soup.find_all('span', attrs={'class': 'price-text'})
+
+
+    data = {} 
+    counter = 0
+    for dep, arriv, stps, price, mr_1, mr_2 in zip(deptimes, arrtimes, stops, prices, mer_dept, mer_arr):
+        array = []
+        array.append(dep.string + ' ' + mr_1.string)
+        array.append(arriv.string + ' ' + mr_2.string)
+        array.append(stps.string[1:])
+        array.append(price.string[1:])
+        data[counter] = array 
+        counter += 1
     
     # data = {}
 
